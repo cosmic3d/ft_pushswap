99416637405cc43dd90ce079b486b1c1b8ff3061
Jesus Navarro romero <jenavarr@cbr8s1.42barcelona.com>
2022-11-20 18:55:24 +0100

    swap finished

--- src/pushswap.c
+++ src/pushswap.c
@@ -6,11 +6,25 @@
 /*   By: jenavarr <jenavarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 20:55:54 by jenavarr          #+#    #+#             */
-/*   Updated: 2022/11/18 21:42:41 by jenavarr         ###   ########.fr       */
+/*   Updated: 2022/11/20 18:07:10 by jenavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../hdrs/pushswap.h"
+
+void	debug_stack(t_stack *stack)
+{
+	t_node	*tmp;
+	
+	tmp = stack->first;
+	while (tmp != NULL)
+	{
+		ft_printf("Value of %d is: %d\n", tmp->index, tmp->val);
+		tmp = tmp->next;
+	}
+	ft_printf("Len of stack a is: %i\n\n", stack->len);
+	return ;
+}
 
 int	main(int argc, char **argv)
 {
@@ -26,10 +40,8 @@ int	main(int argc, char **argv)
 		return (0);
 	ft_init_a(&a, argc, argv);
 	ft_init_b(&b);
-	while (a.first)
-	{
-		ft_printf("Value of %d is: %d\n", a.first->index, a.first->val);
-		a.first = a.first->next;
-	}
-	ft_printf("Len of stack a is: %i", a.len);
+	debug_stack(&a);
+	swap(&a);
+	debug_stack(&a);
+	return (0);
 }
--- src/swap.c
+++ src/swap.c
@@ -6,13 +6,32 @@
 /*   By: jenavarr <jenavarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 21:43:06 by jenavarr          #+#    #+#             */
-/*   Updated: 2022/11/18 21:50:07 by jenavarr         ###   ########.fr       */
+/*   Updated: 2022/11/20 18:55:11 by jenavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../hdrs/pushswap.h"
 
-int swap(t_stack *stack)
+void	swap(t_stack *stack)
 {
-    
+	t_node	*tmp;
+
+	if (stack->len < 2)
+		return ;
+	tmp = stack->first;
+	if (stack->len == 2)
+		stack->last = tmp; //El primero pasa a ser el último/segundo si solo hay dos nodos
+	stack->first = tmp->next; //El segundo pasa a la posición del primero
+	stack->first->back = NULL; //Como el segundo pasa a la posición del primero, su antecesor es NULL
+	tmp->next = stack->first->next; //Antes de cambiar el next del segundo que ahora es primero, se lo asignamos al next del primero que ahora es segundo
+	stack->first->next = tmp; //El segundo que ahora es primero tiene de sucesor al anterior primero
+	tmp->back = stack->first; //El primero que ahora es segundo tiene de antecesor al segundo que ahora es primero
+	return ;
+}
+
+void	swapswap(t_stack *a, t_stack *b)
+{
+	swap(a);
+	swap(b);
+	return ;
 }
