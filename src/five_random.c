8952b1880f88d82f58c4c71f45bb392aeaf8c718
Jesus Navarro romero <jenavarr@car8s5.42barcelona.com>
2022-12-12 14:45:17 +0100

    Explanation added to five_random
    Simulation script created

--- src/five_random.c
+++ src/five_random.c
@@ -6,7 +6,7 @@
 /*   By: jenavarr <jenavarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 19:21:05 by jenavarr          #+#    #+#             */
-/*   Updated: 2022/12/11 20:50:50 by jenavarr         ###   ########.fr       */
+/*   Updated: 2022/12/12 14:44:38 by jenavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,10 @@ void	three_random(t_stack *a, t_stack *b)
 	if (a->last->val < a->first->val && a->first->val < a->last->back->val)
 		return (rotate_a(a, b));
 }
-
+//1- Comprobamos si el stack se halla semiordenado o a varias iteraciones de un movimiento de ordenarse
+//2- Buscamos la manera más rápida de ordenar el stack, que es pusheando los dos más pequeños o el más pequeño y el más grande.
+//3- Realizamos las aciones indicadas por otra función que dictará qué elementos pushear
+//4- Una vez pusheados, llamamos a la función three_random para ordenar esa pareja y consecuentemente traemos a los dos de stack b
 void	five_random(t_stack *a, t_stack *b)
 {
 	if (a->len == 5)
@@ -60,4 +63,9 @@ int	check_smaller(t_stack *stack, int exclude)
 		tmp = tmp->next;
 	}
 	return (smaller->index);
+}
+
+int	how_push(t_stack *a, t_node *smaller, t_node *smaller_2, t_node *bigger)
+{
+	
 }
